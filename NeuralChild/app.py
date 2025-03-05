81177cd80f0918f6a071a0f75991acba2bc5bdcd
Renato Kuipers <renatokuipers1@hotmail.com>
2025-03-05 01:34:54 +0100

    Add initial comments for the Dash dashboard displaying neural network model results

---
+++ NeuralChild/app.py
@@ -0,0 +1,2 @@
+# A Dash dashboard showing the results of the neural network model with metrics, development a chatbot (when the model is trained enough).
+# This should be the window to the models "mind".
