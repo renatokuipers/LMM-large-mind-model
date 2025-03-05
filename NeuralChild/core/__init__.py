19ceac3e7791bc824382d0bb43a867ad38454455
Renato Kuipers <renatokuipers1@hotmail.com>
2025-03-05 13:50:07 +0100

    Add core components package for NeuralChild system with central integration point

---
+++ NeuralChild/core/__init__.py
@@ -0,0 +1,8 @@
+"""
+Core components package for the NeuralChild system.
+Contains the central integration point and key components.
+"""
+
+from .child import NeuralChild
+from .mother import Mother
+from .development import DevelopmentTracker
